9998d69cedc07bcec26d7c1d27b2e77f8283a580
Dann Jewel M De Jesus <comeforthstratos@gmail.com>
2026-01-16 17:17:11 +0800

    Update powerbi.py

--- src/powerbi.py
+++ src/powerbi.py
@@ -21,7 +21,7 @@ left, right = st.columns([3, 1])
 
 with left:
 	st.markdown("### Power BI Report")
-	powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=c8ac3fd7-2da8-4a1f-ad4b-dbd548b17e8c&autoAuth=true&ctid=eb25818e-5bd5-49bf-99de-53e3e7b42630"
+	powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=c8ac3fd7-2da8-4a1f-ad4b-dbd548b17e8c&autoAuth=true&ctid=eb25818e-5bd5-49bf-99de-53e3e7b42630&filterPaneEnabled=false"
 	st.components.v1.iframe(powerbi_url, width=800, height=600)
 
 with right:
